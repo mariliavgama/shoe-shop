f11acd0594e7ac3caa66371ea223a76087f02781
Marilia Gama <mgama@opentable.com>
2024-09-02 01:19:17 -0700

    Login screen first draft

---
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -0,0 +1,78 @@
+package com.udacity.shoestore.screens.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+/**
+ * Login fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false)
+
+        binding.createLoginButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginToWelcome())
+        }
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginToWelcome())
+        }
+
+        return binding.root
+    }
+}
+
+/*
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        val binding: TitleFragmentBinding = DataBindingUtil.inflate(
+                inflater, R.layout.title_fragment, container, false)
+
+        binding.playGameButton.setOnClickListener {
+            findNavController().navigate(TitleFragmentDirections.actionTitleToGame())
+        }
+        return binding.root
+    }
+}
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener { view ->
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overlfow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
+
+ */
---
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.udacity.shoestore.screens.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+/**
+ * Welcome fragment.
+ */
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_welcome, container, false)
+
+        /*binding.playGameButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionTitleToGame())
+        }*/
+        return binding.root
+    }
+}
